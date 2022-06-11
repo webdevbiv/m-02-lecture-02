3ce3190f2ee3e41f4d07c3f3ffd173f99ec1046a
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-06-11 21:04:59 +0300

    form

--- src/ProfileForm/ProfileForm.js
+++ src/ProfileForm/ProfileForm.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 import s from './ProfileForm.module.css';
 
 export default class ProfileForm extends Component {
-    render() {
-        return (
-            <div>Test
+    state = {
+        firstName: '',
+        lastName: ''
+    }
 
-                <form action="">
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state);
+    }
+    render() {
+        const { firstName, lastName } = this.state;
+        console.log(this.state);
+        return (
+            <div>
+                <div>Test</div>
+                <form onSubmit={this.onSubmit}>
                     <div className={s.row}>
                         <div>
-                            <lablel className={s.label}>First Name</lablel>
-                            <input type='text' name='firstName' />
+                            <label className={s.label}>First Name</label>
+                            <input
+                                value={firstName}
+                                type='text'
+                                name='firstName'
+                                onChange={this.onChange}
+                            />
                         </div>
-                        <div>
-                            <lablel className={s.label}>Last Name</lablel>
-                            <input type='text' name='lastName' />
+                        <div className={s.marginLeft}>
+                            <label className={s.label}>Last Name</label>
+                            <input
+                                value={lastName}
+                                type='text'
+                                name='lastName'
+                                onChange={this.onChange}
+                            />
                         </div>
                     </div>
+                    <button type='submit' className={s.button}>Save Profile</button>
                 </form>
             </div>
         )
