4bf8289b9ad6b4982d1329f95b8f514b3ea2bc69
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-06-11 20:20:52 +0300

    form

--- src/ProfileForm/ProfileForm.js
+++ src/ProfileForm/ProfileForm.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import s from './ProfileForm.module.css';
 
 export default class ProfileForm extends Component {
     render() {
         return (
-            <div>Test</div>
+            <div>Test
+
+                <form action="">
+                    <div className={s.row}>
+                        <div>
+                            <lablel className={s.label}>First Name</lablel>
+                            <input type='text' name='firstName' />
+                        </div>
+                        <div>
+                            <lablel className={s.label}>Last Name</lablel>
+                            <input type='text' name='lastName' />
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
